2d43901d282a06a6c3b8862a84c8b05ff053e21e
Rafael Pedrosa <blas_tois@alumnos.ilernasevilla.com>
2023-11-10 13:59:14 +0100

    Commit inicial

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+        // // Función para cambiar el color de fondo
+        // function cambiarColorFondo() {
+        //     // Obtener el color que deseas
+        //     var colorDeseado = '#ff0000'; // Puedes cambiar esto al color que prefieras
+
+        //     // Aplicar el color de fondo al body
+        //     document.body.style.backgroundColor = colorDeseado;
+        // }
+
+        // // Llamar a la función cuando la página se cargue
+        // window.onload = cambiarColorFondo;
