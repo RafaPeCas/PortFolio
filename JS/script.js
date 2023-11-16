d1165c8cce2e1fea5afe278f2a4bee5c1e810aee
Rafael Pedrosa <blas_tois@alumnos.ilernasevilla.com>
2023-11-16 10:50:41 +0100

    Tv implementada

--- JS/script.js
+++ JS/script.js
@@ -1,29 +1,37 @@
-// // Función para cambiar el color de fondo
-// function cambiarColorFondo() {
-//     // Obtener el color que deseas
-//     var colorDeseado = '#ff0000'; // Puedes cambiar esto al color que prefieras
+function cambiar() {
 
-//     // Aplicar el color de fondo al body
-//     document.body.style.backgroundColor = colorDeseado;
-// }
+    if (onOff.classList.contains('off')) {
+        onOff.classList.remove('off');
+        onOff.classList.add('on');
+        derecha.classList.remove("oculto");
+        izquierda.classList.remove("oculto");
+        filtro.classList.remove("oculto");
+    } else {
+        onOff.classList.remove('on');
+        onOff.classList.add('off');
+        derecha.classList.add("oculto");
+        izquierda.classList.add("oculto");
+        filtro.classList.add("oculto");
+    }
 
-// // Llamar a la función cuando la página se cargue
-// window.onload = cambiarColorFondo;
+    if(tv.classList.contains("oculto")){
+        tv.classList.remove("oculto");
+    }else{
+        tv.classList.add("oculto")
+        tv.style.background = "red"
+    }
 
-function toggleDropdown() {
-        var dropdown = document.getElementById("myDrop");
-        dropdown.style.display = (dropdown.style.display === 'block') ? 'none' : 'block';
-}
+    telesound.play();
 
-// Cierra el dropdown si el usuario hace clic fuera de él
-window.onclick = function (event) {
-        if (!event.target.matches('.dropbtn')) {
-                var dropdowns = document.getElementsByClassName("drop-content");
-                for (var i = 0; i < dropdowns.length; i++) {
-                        var openDropdown = dropdowns[i];
-                        if (openDropdown.style.display === 'block') {
-                                openDropdown.style.display = 'none';
-                        }
-                }
-        }
+};
+
+let i=0;
+function canales(gax){
+    let imagenes = ["img/Yo.jpg", "img/mauro-Escalado.jpeg"];
+    i+=gax;
+    if (i>imagenes.length-1) i=0;
+    if (i<0) i= imagenes.length-1;
+    console.log(i+" Va a actuar")
+    canal.src= imagenes[i];
+    nextsound.play();
 }
