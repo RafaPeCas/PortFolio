74142ddd4effa7c27fa8b0fe8a10eecc92525f4e
Rafael Pedrosa <blas_tois@alumnos.ilernasevilla.com>
2023-11-20 13:41:05 +0100

    Sección proyectos terminado

--- JS/script.js
+++ JS/script.js
@@ -14,9 +14,9 @@ function cambiar() {
         filtro.classList.add("oculto");
     }
 
-    if(tv.classList.contains("oculto")){
+    if (tv.classList.contains("oculto")) {
         tv.classList.remove("oculto");
-    }else{
+    } else {
         tv.classList.add("oculto")
         tv.style.background = "red"
     }
@@ -25,13 +25,23 @@ function cambiar() {
 
 };
 
-let i=0;
-function canales(gax){
-    let imagenes = ["img/Yo.jpg", "img/mauro-Escalado.jpeg"];
-    i+=gax;
-    if (i>imagenes.length-1) i=0;
-    if (i<0) i= imagenes.length-1;
-    console.log(i+" Va a actuar")
-    canal.src= imagenes[i];
-    nextsound.play();
+let i = 0;
+function canales(gax) {
+    let imagenes = {
+        "img/minas.png": ["Buscador de minas", "https://view.genial.ly/638c6eb5bbff690017976335/presentation-presentacion-proyeccion"],
+        "img/misterio_nuestro.png": ["Misterio Nuestro", "https://view.genial.ly/64122587b692cc001366b7a7/presentation-misterio-nuestro"],
+        "img/nuestro_misterio.png": ["Nuestro Misterio", "https://view.genial.ly/646cecebf438b10011c6b7dc/presentation-nuestro-misterio"]
+    };
+    let keys = Object.keys(imagenes);
+
+    i += gax;
+
+    if (i > keys.length - 1) i = 0;
+    if (i < 0) i = keys.length - 1;
+
+    let imagenActual = keys[i];
+
+    canal.src = imagenActual;
+    descripciontv.textContent = imagenes[imagenActual][0];
+    enlace.href=imagenes[imagenActual][1];
 }
